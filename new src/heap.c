ae272c9af6fd31053df7bb1d627a07a01e83af3b
Hampus Toft <hampus.toft.1883@student.uu.se>
2025-01-21 22:49:50 +0100

    refact: moved creation and deleation into heap.c

--- new src/heap.c
+++ new src/heap.c
@@ -7,9 +7,43 @@
 #include "allocation_map.h"
 #include "heap_internal.h"
 
+heap_t *
+create_heap (size_t bytes, bool unsafe_stack, float threshold)
+{
+  /* Allocate heap struct on the heap.  */
+  heap_t *heap = malloc (sizeof (heap_t));
+
+  /* Align size of heap to the allocation maps BYTE_DENSITY */
+  size_t aligned_size = round_up_to_alignment (bytes, ALLOCATION_MAP_DENSITY);
+
+  /* Set fields:  */
+  heap->gc_threshold = threshold;
+  heap->is_unsafe_stack = unsafe_stack;
+  heap->size = aligned_size;
+  heap->alloc_map = create_allocation_map (aligned_size);
+  heap->heap_start = calloc (aligned_size, sizeof (char));
+  heap->next_empty_mem_segment = heap->heap_start;
+  heap->used_bytes = 0;
+
+  return heap;
+}
+
+void
+destroy_heap (heap_t *heap)
+{
+  free (heap->heap_start);
+  free (heap);
+}
+
 size_t
 num_free_bytes (heap_t *heap)
 {
   size_t used = num_allocated_bytes (heap->alloc_map);
   return heap->size - used;
+}
+
+void
+toggle_direction (heap_t *heap)
+{
+  heap->left_to_right = !heap->left_to_right;
 }
--- new src/heap.h
+++ new src/heap.h
@@ -3,6 +3,7 @@
  */
 
 #pragma once
+#include <stdbool.h>
 #include <stdint.h>
 
 /**
@@ -11,16 +12,40 @@
 typedef struct heap heap_t;
 
 /**
+ * @brief Creates a heap with given atleast given size and sets stack safety
+ * and threshold
+ * @return Newly created stack.
+ */
+heap_t *create_heap (size_t bytes, bool unsafe_stack, float threshold);
+
+/**
+ * @brief Destroys the given heap.
+ */
+void destroy_heap (heap_t *heap);
+
+/**
  * @brief Calculates how much space is available in the heap using the
  * allocation map. This estimate excludes any data that is used for
  * alignments.
  *
  * @note Whilst the number of available bytes might be high, this dose
- * not equal such a object can be allocated. This is due to fragmentation in
- * the heap leaving spots which will be reported as free but might be impossible
- * to allocate a object on.
+ * not equal such a object can be allocated. This is due to fragmentation
+ * in the heap leaving spots which will be reported as free but might be
+ * impossible to allocate a object on.
  *
  * @param heap Heap to count available space in.
  * @return Number of bytes that can be allocated.
  */
 size_t num_free_bytes (heap_t *heap);
+
+/**
+ * @brief changes direction of the heap.
+ * when heap is set to left_to_right = true the bump pointer will travel from
+ * start to the end. When set to false it will instead travel from end to
+ * start. This helps by avoid allocating in areas that have been forwarded
+ * during h_gc.
+ * @param heap Heap to change allocation direction in
+ * @note when changing direction the bump pointer will always travel from the
+ * start of the edge.
+ */
+void toggle_direction (heap_t *heap);
