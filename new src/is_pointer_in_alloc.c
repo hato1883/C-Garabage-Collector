bc534303964aab8d900f559854ed7155fecb80ba
Hampus Toft <hampus.toft.1883@student.uu.se>
2025-01-22 09:27:26 +0100

    refact: Updated files to use void * instead of uintptr_t

--- new src/is_pointer_in_alloc.c
+++ new src/is_pointer_in_alloc.c
@@ -3,8 +3,6 @@
  * heap.
  */
 
-// TODO: MODIFY FILE
-
 #include <assert.h>
 #include <stdbool.h>
 #include <stdint.h>
@@ -14,63 +12,56 @@
 #include "heap_internal.h"
 #include "is_pointer_in_alloc.h"
 
-/*
- * TODO: Right now we need to check the entire heap, it would be good if had
- * a starting location narrowing the search down.
- * Checks if a pointer address (or maybe not) is in a heaps range.
- * @param ptr_addr - unknown pointer (or maybe not).
+/**
+ * @brief Checks if a pointer address (or maybe not) is in a heaps range.
+ * @param pointer - unknown pointer (or maybe not).
  * @param the_heap - the_heap on which we want to check if the pointer is in
- * @return true if the value is in range of the heap address, false if not.  */
+ * @return true if the value is in range of the heap address, false if not.
+ */
 bool
-is_in_range (uintptr_t ptr_addr, heap_t *the_heap)
+is_in_range (void *pointer, heap_t *the_heap)
 {
-  /* Compare the address of the heap to the uintptr_t */
-  uintptr_t beginning = (uintptr_t)the_heap->heap_start;
-  uintptr_t end = beginning + the_heap->size;
+  /* Compare the address of the heap to the ptr */
+  void *beginning = the_heap->heap_start;
+  void *end = beginning + the_heap->size;
 
   /* Simply check if it is in range  */
-  if (ptr_addr >= beginning && ptr_addr < end)
-    {
-      return true;
-    }
-
-  return false;
+  return (pointer >= beginning && pointer < end);
 }
 
-/* Checks whether a pointer (might not be one though) is in an active memory.
- * @param ptr_addr - the supposed pointer.
- * @param the_heap - the_heap in which we check if ptr_address might be active
+/**
+ * @brief Checks whether a pointer (might not be one though) is in an active
+ * memory.
+ * @param pointer - the supposed pointer.
+ * @param the_heap - the_heap in which we check if pointer might be active
  * in.
- * @return true if the ptr_addr is in active memory, false if not.  */
+ * @return true if the pointer is in active memory, false if not.
+ */
 bool
-is_in_allocated_region (uintptr_t ptr_addr, heap_t *the_heap)
+is_in_allocated_region (void *pointer, heap_t *the_heap)
 {
-  if (!is_in_range (ptr_addr, the_heap))
+  if (!is_in_range (pointer, the_heap))
     {
       return false;
     }
 
-  size_t diff = ptr_addr - (uintptr_t)the_heap->heap_start;
-  char *alloc_map = the_heap->alloc_map;
+  size_t diff = pointer - the_heap->heap_start;
+  char *map = the_heap->alloc_map;
 
-  assert (alloc_map != NULL && "The alloc map is null.");
+  assert (map != NULL && "The alloc map is null.");
+  char status = map[find_index_in_alloc_map (diff)] & create_bitmask (diff);
 
-  if (alloc_map[find_index_in_alloc_map (diff)] & create_bitmask (diff))
-    {
-      return true;
-    }
-
-  return false;
+  return status != 0;
 }
 
 /**
- * Checks if a uintptr_t is an address that is within a heap.
- * @param ptr_addr - the (supposed) address we want to check.
+ * @brief Checks if a pointer is an address that is within a heap.
+ * @param pointer - the (supposed) address we want to check.
  * @param the_heap - the heap we are checking the address in.
  * @return true if address is within heap.
  */
 bool
-is_heap_pointer (uintptr_t ptr_addr, heap_t *the_heap)
+is_heap_pointer (void *pointer, heap_t *the_heap)
 {
-  return is_in_allocated_region (ptr_addr, the_heap);
+  return is_in_allocated_region (pointer, the_heap);
 }
